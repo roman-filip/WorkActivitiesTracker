2c598a43f7e0d486db2c1f60d7bfcc876009eb07
Roman FILIP <dicky@email.cz>
2020-07-19 12:23:17 +0200

    Added Blazor WEB application

---
+++ WorkActivitiesTracker.Web/Shared/Models/Timesheet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkActivitiesTracker.Web.Shared.Models
+{
+    public class Timesheet
+    {
+        public DateTime Date { get; set; }
+
+        public List<TimesheetEntry> Entries { get; set; }
+    }
+
+    public class TimesheetEntry
+    {
+        public DateTime? StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+    }
+}
